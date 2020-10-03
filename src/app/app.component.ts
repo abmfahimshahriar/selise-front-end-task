a4abeb5836488253cbc4cc1e0505e7d93f7d992a
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-10-03 12:39:20 +0600

    overlay fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,19 +19,22 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.overlayContainer.getContainerElement().classList.add(this.theme);
   }
-  changeTheme() {
-    this.theme = (this.theme === 'light-theme') ? 'dark-theme' : 'light-theme' ;
-  }
-
-  // changeTheme(event): void {
-  //   if (this.overlay.classList.contains('dark-theme')) {
-  //     this.overlay.classList.remove('dark-theme');
-  //     this.overlay.classList.add('light-theme');
-  //   } else if (this.overlay.classList.contains('light-theme')) {
-  //     this.overlay.classList.remove('light-theme');
-  //     this.overlay.classList.add('dark-theme');
-  //   } else {
-  //     this.overlay.classList.add('light-theme');
-  //   }
+  // changeTheme() {
+  //   this.theme = (this.theme === 'light-theme') ? 'dark-theme' : 'light-theme' ;
   // }
+
+  changeTheme(event): void {
+    if (this.overlay.classList.contains('dark-theme')) {
+      this.overlay.classList.remove('dark-theme');
+      this.overlay.classList.add('light-theme');
+      this.theme = 'light-theme';
+    } else if (this.overlay.classList.contains('light-theme')) {
+      this.overlay.classList.remove('light-theme');
+      this.overlay.classList.add('dark-theme');
+      this.theme = 'dark-theme';
+    } else {
+      this.overlay.classList.add('light-theme');
+      this.theme = 'light-theme';
+    }
+  }
 }
